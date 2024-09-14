41c5325e36615622478ca1bdb8e1921f5a55b856
Stalin Alejandro Amagua Sandobalín <jueltter@users.noreply.github.com>
2024-09-14 10:39:34 -0500

    initial commit

---
+++ src/test/java/ec/com/samagua/libreria01eurekaserver/Libreria01EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.samagua.libreria01eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Libreria01EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
